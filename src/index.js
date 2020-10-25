87df2e879d80eb41a60218ad660a410448c20850
ildar_test <attacker.cs@gmail.com>
2020-10-26 00:55:57 +0500

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let temp = str;
+    for (let i = 0; i < str.length; i++) {
+        for (let br of bracketsConfig) {
+            let fullBracket = br[0] + br[1];
+            if (temp.includes(fullBracket))
+                temp = temp.replace(fullBracket, "");
+        }
+    }
+
+    if (temp.length > 0) return false;
+    else return true;
+};
+
+/*
+const config6 = [
+    ["1", "2"],
+    ["3", "4"],
+    ["5", "6"],
+    ["7", "7"],
+    ["8", "8"],
+];
+module.exports(
+    "111115611111111222288888822225577877778775555666677777777776622222",
+    config6
+);
+*/
+
+const config3 = [
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"],
+];
+
+module.exports("([{}])", config3);
